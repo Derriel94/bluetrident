2c5400e3b33342dac4c86d130eeb287f6e0b36f9
Derriel Collins <derrielcollins96@gmail.com>
2022-10-05 15:34:52 -0500

    added a search bar for filtering blogs

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -6,6 +6,7 @@ import { db } from "../firebaseConfig.js";
 
 export default function Articles() {
 	const [articles, setArticles] = useState([]);
+	let [keyword, setKeyword] = useState('');
 	useEffect(()=>{
 		const articleRef = collection(db, "blogs");
 		const q = query(articleRef, orderBy("createdAt"));
@@ -18,13 +19,42 @@ export default function Articles() {
 			console.log(articles);
 		});
 	},[])
+
+	const handleChange = (e) => {
+			setKeyword(e.target.value);	
+	}
+
 	return (
 		<div>
+			<div>
+				<p>Enter keyword</p>
+				<input type="text" name="keyword" onChange={handleChange} />
+			{/*		{articles.filter((val)=>{
+						if(keyword == "") {
+							return val
+						} else if (val.title.toLowerCase().includes(keyword.toLowerCase())) {
+							return val
+						}
+					}).map((val, key) => {
+					return (
+					<div key={key}>
+						<p>{val.title}</p>
+					</div>
+				)
+			})
+				}*/}
+			</div>
 			{
 				articles.length === 0 ? (
 					<p>No articles found!</p>
 				):(
-				articles.map(({id, title, description, imageUrl, createdAt}) =>{
+					articles.filter((val)=>{
+						if(keyword == "") {
+							return val
+						} else if (val.title.toLowerCase().includes(keyword.toLowerCase())) {
+							return val
+						}
+					}).map(({id, title, description, imageUrl, createdAt}) =>{
 					return <div key={id} className="article">
 								<h1>{title}</h1>
 								<img src = {imageUrl} alt={title} style={{height: 180, width: 180}} />
